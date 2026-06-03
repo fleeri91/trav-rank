e1df6dc0f12558066df876c322430ce738ff58ad
Flemming Eriksson <fleeri91@gmail.com>
2026-06-03 17:38:40 +0200

    import fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import QueryProvider from "@/providers/QueryProvider";
+import QueryProvider from "@/providers/query-provider";
 
 import "./globals.css";
 
